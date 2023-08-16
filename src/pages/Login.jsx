6f5a259e1e1206bb3e0303f7cc4edfd90dd6a242
Tim <timward1985@gmail.com>
2023-08-16 10:24:44 -0500

    loginbg

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom'
 
 export default function Login() {
     return (
-        <div className="pt-36 w-[50%] md:w-[20%] m-auto">
+      <div className="pt-36 w-[50%] md:w-[20%] m-auto">
+        <img src="/moviebg.jpg" className="flex absolute -z-10 w-full h-full left-0 top-0 opacity-20" />
         <div className="border-2 border-sky-500 rounded-md text-center text-white">
             <h1 className="text-3xl font-bold text-sky-500 my-8">Log In</h1>
             <div className="flex flex-col mx-10 text-black">
@@ -14,7 +15,7 @@ export default function Login() {
               <Link to="/password" ><button className="mb-8 text-white text-xs">Forget password?</button></Link>
             </div>
             <button className="bg-sky-500 rounded-md w-[80px] h-[35px] mb-7 hover:bg-sky-600 hover:border-gray-600 border-2">Log In</button>
-          </div>
         </div>
+      </div>
     )
 }
