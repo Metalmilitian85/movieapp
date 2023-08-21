f8d328c3c874396dac5fee5378a7a41815e61c43
Tim <timward1985@gmail.com>
2023-08-21 11:23:21 -0500

    almostdone

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Savedshows from '../components/Savedshows'
 
 export default function Account() {
   return (
-    <div>
-        <h1>Account</h1>
+    <div className="w-full text-white">
+        <img src="/moviebg.jpg" className="flex absolute -z-10 w-full h-full left-0 top-0 opacity-20" />
+        <h1 className="text-center">Account</h1>
+        <Savedshows />
     </div>
   )
 }
