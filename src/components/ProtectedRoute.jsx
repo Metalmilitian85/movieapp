7d74ffc8983b94943e1d47f7e4ccd962c10abdd6
Tim <timward1985@gmail.com>
2023-08-21 09:26:31 -0500

    protected

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -10,9 +10,6 @@ export default function ProtectedRoute({children}) {
         return <Navigate to="/" />
     } else {
         return children
-    }
+    };
 
-  return (
-    <div>ProtectedRoute</div>
-  )
 }
